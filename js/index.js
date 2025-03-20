57289e74eacc041066e9388bf9b115ddb2b8dd21
TheLuis <luisdavidgilcabarcas47@gmail.com>
2025-03-20 10:22:37 -0500

    Interfaz terminada

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 'use strict'
 // <    >  =>
 
-    window.addEventListener('DOMContentLoaded', () => {
-        console.log('Web funcionando!');
-    });
+window.addEventListener('DOMContentLoaded', () => {
+    
+});
